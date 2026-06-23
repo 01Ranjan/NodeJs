71379392fea3d010eaf15f85a3f9151eba0c45cf
01Ranjan <ranjankumar459688@gmail.com>
2026-06-23 12:35:25 +0530

    add .gitignore, package.json, and initial index.js file

---
+++ 02node.package/02index.js
@@ -0,0 +1,6 @@
+// to create package.jso -------> npm init
+// now installing 3rd party module  by -----> npm i package_name
+
+const fs =require('fs');
+
+// now the third party is installed and can be use
