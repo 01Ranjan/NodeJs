80225fb2b38dfba5fb4f32b0fefe6fbc8aa4ae83
01Ranjan <ranjankumar459688@gmail.com>
2026-06-23 12:08:57 +0530

    add initial implementation of file system module and read file functionality

---
+++ 01index.js
@@ -0,0 +1,18 @@
+ // modules
+
+ // build in module
+ // 3rd party module
+ // custom module( my module);
+
+
+ // file module
+ 
+
+ const fs=require('fs');
+
+//  console.log(fs);
+
+ const containnt=fs.readFileSync('01.txt','utf-8')
+
+//  console.log(containnt);
+ 
