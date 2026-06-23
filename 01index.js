da7221ce8b29dd98a9ce8ec39a0f3402539ffbfe
01Ranjan <ranjankumar459688@gmail.com>
2026-06-23 12:21:58 +0530

    add comments to clarify module loading and file execution context

--- 01index.js
+++ 01index.js
@@ -15,4 +15,16 @@
  const containnt=fs.readFileSync('01.txt','utf-8')
 
 //  console.log(containnt);
+
+
+// (function(exports, require, module, __filename, __dirname) {
+//     Your actual file code lives inside here!
+// });
+
+// require('fs')---->try to find buildin module, 3rd party module if not throw error
+
+
+//require('./fs')--> now it try to find the costum module
+
+
  
